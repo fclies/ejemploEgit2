f1b3dacc35cef8ecf66d79100f7a9c0589c5bfd7
fclies <fclies@gmail.com>
2019-02-04 19:01:42 +0100

    Version que incluye la funcionalidad resta

--- TestEgit2/src/Ejemplo.java
+++ TestEgit2/src/Ejemplo.java
@@ -3,13 +3,15 @@ public class Ejemplo {
 
 	public static void main(String[] args) {
 		/*
-		 * Programa Java que realize lo siguiente: declarar una variable N de tipo int, una variable
-		 * A de tipo double y una variable C de tipo char y asigna a cada una un valor. A continuación
-		 * muestra por pantalla: el valor de cada variable, la suma de N + A, la diferencia de A - N
-		 * y el valor numérico correspondiente al carácter que contiene la variable C.
-		*/
+		 * Programa Java que realize lo siguiente: declarar una variable N de
+		 * tipo int, una variable A de tipo double y una variable C de tipo char
+		 * y asigna a cada una un valor. A continuación muestra por pantalla: el
+		 * valor de cada variable, la suma de N + A, la diferencia de A - N y el
+		 * valor numérico correspondiente al carácter que contiene la variable
+		 * C.
+		 */
 
-		int n= 4;
+		int n = 4;
 		double a = 7.2;
 		char c = 'n';
 
@@ -18,6 +20,13 @@ public class Ejemplo {
 		System.out.println("N = " + n);
 		System.out.println("A = " + a);
 		System.out.println("C = " + c);
+
+		// Mostrar la diferencia de A - N:
+
+		double resta = a - n;
+
+		System.out.println(
+				"La resta de la variable A (" + a + ") más la variable N (" + n + ") da como resultado " + resta);
 
 	}
 
